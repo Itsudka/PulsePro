b41db87ae99c14ac43e3693b41067c64f62dfed8
Juan David Lozano <judalo777@gmail.com>
2025-04-27 18:00:56 -0500

    Autentication
    Configuration - Clerk

--- pulsepro/src/app/page.tsx
+++ pulsepro/src/app/page.tsx
@@ -1,8 +1,11 @@
+import { SignInButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div>
+      Homepage
+    </div>
   )
 }
 
