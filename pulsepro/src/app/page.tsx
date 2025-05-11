e12684be5888bc70910480fe10f7b960a0d1aa34
Juan David Lozano <judalo777@gmail.com>
2025-05-10 19:09:19 -0500

    First part -  Home screen
    Nav bar - Done
    Home page - 50%

--- pulsepro/src/app/page.tsx
+++ pulsepro/src/app/page.tsx
@@ -1,10 +1,102 @@
-import { SignInButton } from '@clerk/nextjs'
+
+import { Button } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
-      Homepage
+    <div className='flex flex-col min-h-screen text-foreground overflow-hidden'>
+      <section className='relative z-10 py-24 flex-grow'>
+        <div className='container mx-auto px-4'>
+          <div className='grid grid-cols-1 lg:grid-cols-12 gap-8 items-center relative'>
+            {/* CORNER DECORATION */}
+            <div className='absolute -top-10 left-0 w-40 h-40 border-l-2 border-t-2 '/>
+
+            {/* LEFT SIDE CONTENT */}
+
+            <div className='lg:col-span-7 space-y-8 relative'>
+              <h1 className='text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight'>
+                <div>
+                  <span className='text-foreground'>Sculpt</span>
+                </div>
+                <div>
+                  <span className='text-primary'>Your body</span>
+                </div>
+                <div className='pt-2'>
+                  <span className='text-foreground'>With Intelligent</span>
+                </div>
+                <div className='pt-2'>
+                  <span className='text-foreground'>Innovation</span>
+                </div>
+              </h1>
+
+            {/* SEPARATOR LINE */}
+              <div className='h-px w-full bg-gradient-to-r from-primary via-second to-primary opacity-50'>
+
+              </div>
+
+              <p className='text-xl text-muted-foreground w-2/3'>
+                Talk to our AI assistant and get personalized diet plans and workout routines designed just for you.
+              </p>
+
+            {/* STATS */}
+
+              <div className='flex items-center gap-10 py-6 font-mono'>
+                <div className='flex flex-col'>
+                  <div className='text-2xl text-primary'>500+</div>
+                  <div className='text-xs uppercase tracking-wider'>ACTIVE USERS</div>
+                </div>
+
+                <div className='h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent'></div>
+                <div className='flex flex-col'>
+                  <div className='text-2xl text-primary'>3min</div>
+                  <div className='text-xs uppercase tracking-wider'> GENERATION </div>
+                </div>
+
+                <div className='h-12 w-px bg-gradient-to-b from-transparent via-border to-transparent'></div>
+                <div className='flex flex-col'>
+                  <div className='text-2xl text-primary'>100%</div>
+                  <div className='text-xs uppercase tracking-wider'>PERSONALIZED</div>
+                </div>
+              </div>
+
+              {/* BUTTON */}
+
+              <div className='flex flex-col sm:flex-row gap-4 pt-6'>
+                <Button size="lg" asChild className='overflow-hidden bg-primary text-primary-foreground px-8 py-6 text-lg font-medium'>
+                  <Link href={"/generate-program"} className='flex items-center font-mono'>
+                    Build Your Program
+                    <ArrowRight className="ml-2 size-5"/>
+                  </Link>
+                </Button>
+              </div>
+            </div>
+
+          {/* RIGHT SIDE CONTENT */}
+            <div className='lg:col-span-5 relative'>
+              {/* CORNER PIECES */}
+              <div className='absolute -inset-4 pointer-events-none'>
+                <div className='absolute top-0 left-0 w-16 h-16 border-l-2 border-t-2 border-border'/>
+                <div className='absolute top-0 right-0 w-16 h-16 border-r-2 border-t-2 border-border'/>
+                <div className='absolute bottom-0 left-0 w-16 h-16 border-l-2 border-b-2 border-border'/>
+                <div className='absolute bottom-0 right-0 w-16 h-16 border-r-2 border-b-2 border-border'/>                
+              </div>
+
+              {/* IMAGE CONTENT */}
+
+              <div className='relative aspect-square max-w-lg mx-auto'>
+                <div className='relative overflow-hidden rounded-lg bg-cyber-black'>
+                  <img src="/hero-ai2.png" className='size-full object-center'/>
+                </div>
+              </div>
+            </div>
+
+
+        </div>
+      </div>
+         
+      </section>
     </div>
   )
 }
